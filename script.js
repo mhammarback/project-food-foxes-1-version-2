2c51e7a1f7fcecad5f5b8b725dc8d2201e51c17c
MindstormingAB <s.elander@mindstorming.se>
2020-09-30 11:24:51 +0200

    Rating filter

--- script.js
+++ script.js
@@ -11,25 +11,38 @@ const searchRequest = new Request(SEARCH_API_URL, {
   headers: headers
 })
 
-const restaurants = document.getElementById("restaurants")
+const restaurants = document.getElementById("restaurants");
+const filterButton = document.getElementById('filterSubmit')
 
 fetch(searchRequest)
   .then(response => response.json())
   .then(data => {
-    console.log(data.restaurants);
-    const newRestaurants = data.restaurants.map(item => {
-      const name = item.restaurant.name;
-      const address = item.restaurant.location.address;
-      const averageCost = item.restaurant.average_cost_for_two;
-      const rating = item.restaurant.user_rating.aggregate_rating;
-      const image = item.restaurant.thumb;
-      return { name, address, averageCost, rating, image };
-    });
+    const restaurantArray = data.restaurants;
+    console.log(restaurantArray);
+    getRestaurantInfo(restaurantArray);
     console.log(newRestaurants);
     newRestaurants.forEach((restaurant) => {
       restaurants.innerHTML += generateHTMLForRestaurants(restaurant);
     });
   });
+
+const checkElements = document.querySelectorAll('.rating');
+const delivery = document.getElementById
+
+
+displayFilteredRestaurants = (array) => {
+  let filteredRestaurants = [];
+  checkElements.forEach((rating) => {
+    if (rating.checked) {
+      const partOfFilteredRestaurants = array.filter(item => {
+        return Math.round(item.rating) === parseInt(rating.value);
+      });
+      filteredRestaurants = [...filteredRestaurants, ...partOfFilteredRestaurants];
+    }
+  })
+  console.log(filteredRestaurants);
+  return filteredRestaurants;
+};
 
 const generateHTMLForRestaurants = (array1) => {
   let restaurantHTML = "";
@@ -40,3 +53,23 @@ const generateHTMLForRestaurants = (array1) => {
   restaurantHTML += `<img src=${array1.image}>`;
   return restaurantHTML;
 };
+
+filterButton.addEventListener('click', () => {
+  const filteredRestaurants = displayFilteredRestaurants(newRestaurants);
+  restaurants.innerHTML = '';
+  filteredRestaurants.forEach((restaurant) => {
+    restaurants.innerHTML += generateHTMLForRestaurants(restaurant);
+  });
+})
+
+let newRestaurants = [];
+const getRestaurantInfo = (array) => {
+  array.forEach(item => {
+    const name = item.restaurant.name;
+    const address = item.restaurant.location.address;
+    const averageCost = item.restaurant.average_cost_for_two;
+    const rating = parseFloat(item.restaurant.user_rating.aggregate_rating);
+    const image = item.restaurant.thumb;
+    newRestaurants.push({ name, address, averageCost, rating, image });
+  });
+}
